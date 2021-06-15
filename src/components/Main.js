c6f1c7903527e837fddede5f8dd22b66d17c9bfe
Наталья Михалева <natalamihaleva@MacBook-Air-Natala.local>
2021-06-15 13:14:32 +0300

    node_env is  added

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Main(props) {
-  const smallUrl = `http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
-  const bigUrl = `http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32`;
+  //константы
+  const SMALL_URL =
+     process.env.NODE_ENV === "production"
+      ? `https://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`
+      : `http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
+  const BIG_URL =
+    process.env.NODE_ENV === "production"
+      ? `https://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32`
+      : `http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32`;
+  // const smallUrl = `http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
+  // const bigUrl = `http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32`;
   return (
     <div
       style={{ display: "flex", justifyContent: "center", padding: "50px 0" }}
@@ -12,22 +21,22 @@ function Main(props) {
         <button
           type="button"
           className="btn btn-outline-secondary"
-          onClick={() => props.handleBtnClick(smallUrl)}
+          // 
+          onClick={() => props.handleBtnClick(SMALL_URL)}
         >
           Вывести малый массив данных
         </button>
       </Link>
-     <Link to="/table">
-     <button
-        type="button"
-        className="btn btn-outline-secondary"
-        onClick={() => props.handleBtnClick(bigUrl)}
-      >
-        Вывести большой массив данных
-      </button>
-     </Link>
-      
-      
+      <Link to="/table">
+        <button
+          type="button"
+          className="btn btn-outline-secondary"
+          // 
+          onClick={() => props.handleBtnClick(BIG_URL)}
+        >
+          Вывести большой массив данных
+        </button>
+      </Link>
     </div>
   );
 }
